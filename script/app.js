e32b1615016027b124057a591d6cbcbe4ad19418
raj68518 <raj68518@gmail.com>
2020-10-12 17:07:03 +0530

    canvas and images added

--- script/app.js
+++ script/app.js
@@ -1,8 +1,15 @@
 const file = document.getElementById('file');
-const uploadBtn = document.getElementById('uploadBtn')
-
+const uploadBtn = document.getElementById('uploadBtn');
+const canvas = document.getElementById('canvas');
+var ctx = canvas.getContext("2d");
 
 uploadBtn.addEventListener("click", ()=>{
     console.log("hello");
 })
+
+ctx.toDataURL("http://localhost:5500/images/smp.png")
+
+console.log(ctx);
+
+
 
