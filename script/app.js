9cf403e442c5fb3cef0e491450f5309a0ea077a0
raj68518 <raj68518@gmail.com>
2020-10-14 16:59:11 +0530

    some change done

--- script/app.js
+++ script/app.js
@@ -1,15 +1,29 @@
 const file = document.getElementById('file');
 const uploadBtn = document.getElementById('uploadBtn');
 const canvas = document.getElementById('canvas');
-var ctx = canvas.getContext("2d");
+var ctx = canvas.getContext('2d');
+
+let arraBf;
+
+file.addEventListener('change', handleFile)
+
+function handleFile(){
+    var firstFile = this.files[0];
+    var reader = new FileReader();
+    var imgElem = document.createElement('img')
+    ctx.drawImage(reader.readAsArrayBuffer(firstFile), 0,0)
+}
+
 
 uploadBtn.addEventListener("click", ()=>{
-    console.log("hello");
+    ctx.drawImage(arraBf, 0,0)
 })
 
-ctx.toDataURL("http://localhost:5500/images/smp.png")
 
 console.log(ctx);
+console.log(canvas);
+
+// ctx.drawImage("http://localhost/imgages/smp.png", 0, 0)
 
 
 
